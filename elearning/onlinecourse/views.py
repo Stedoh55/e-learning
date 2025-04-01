e3c3991c8929462d1bb3934922f8d77ef25b8d50
Stedoh <stevensimoni55@gmail.com>
2025-04-01 19:44:11 +0300

    Implementing aggregate functions to models and ordering results

--- elearning/onlinecourse/views.py
+++ elearning/onlinecourse/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Course
+from django.db.models import Sum, Avg, Max, Min
 
 # Create your views here.
 def index(request):
@@ -9,9 +10,21 @@ def index(request):
 #     return render(request, 'onlinecourse/courses.html')
 
 def course_list(request):
-    courses = Course.objects.all()
+    courses = Course.objects.order_by('name') #Ordering Courses by names
+    number = Course.objects.count()
+    students = Course.objects.aggregate(Sum('total_enrollment'))['total_enrollment__sum'] or 0
+    average = round(Course.objects.aggregate(Avg('total_enrollment'))['total_enrollment__avg']) or 0
+    maximum_students = Course.objects.aggregate(Max('total_enrollment'))['total_enrollment__max'] or 0
+    minimum_students = Course.objects.aggregate(Min('total_enrollment'))['total_enrollment__min'] or 0
+    
     context = {
-        'courses': courses
+        'courses': courses,
+        'number': number,
+        'students': students,
+        'average': average,
+        'maximum_students': maximum_students,
+        'minimum_students': minimum_students,
+
     }
     return render(request, 'onlinecourse/courses.html', context)
     
