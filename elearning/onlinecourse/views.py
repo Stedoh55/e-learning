986c8e2bc7e60e4fa1ce0d0cad03199223be7477
Stedoh <stevensimoni55@gmail.com>
2025-04-03 11:56:30 +0300

    Implementing sign up and signin functionalities and styling

--- elearning/onlinecourse/views.py
+++ elearning/onlinecourse/views.py
@@ -1,14 +1,17 @@
 from django.shortcuts import render,redirect
+from django.http import HttpResponseRedirect
 from django.views import generic
 from .models import Course
 from django.db.models import Sum, Avg, Max, Min
 from django.contrib.auth import authenticate,login,logout
 from django.contrib import messages
 from .forms import SignupForm
+from django.contrib.auth.models import User
+from .models import CustomUser
 
 # Create your views here.
 def index(request):
-    return render(request, 'onlinecourse/index.html')
+    return render(request, 'onlinecourse/pages/index.html')
 
 # def courses(request):
 #     return render(request, 'onlinecourse/courses.html')
@@ -33,12 +36,12 @@ def course_list(request):
         'minimum_students': minimum_students,
 
     }
-    return render(request, 'onlinecourse/courses.html', context)
+    return render(request, 'onlinecourse/pages/courses.html', context)
 
 # Using Generic Views for easy request calls        (3 Steps)
 class CourseView(generic.DetailView):               #1: Extends detailview
     model = Course                                  #2: Define model
-    template_name = 'onlinecourse/course_detail.html'     #3: Define template
+    template_name = 'onlinecourse/pages/course_detail.html'     #3: Define template
     context_object_name = 'course'
     
 def login_request(request):
@@ -50,35 +53,69 @@ def login_request(request):
         #Authenticate user
         user = authenticate(request, username=username, password=password)
 
-        #Login user
+        #Check for Login Credentials
         if user is not None:
-            login(request,user)
             messages.success(request, "Login successful!")
-            return redirect('dashboard')
+            login(request,user)
+           
         else:
-            messages.error(request, "Invalid username or password.")
-    return render(request, 'onlinecourse/login.html')
+             return render(request, 'onlinecourse/pages/login.html',{"error_message": "Incorrect Username or Password."})
+          
+    return render(request, 'onlinecourse/pages/login.html')
 
 def dashboard(request):
-    return render(request, 'onlinecourse/dashboard.html')
+    return render(request, 'onlinecourse/pages/dashboard.html')
 
 def logout_request(request):
     logout(request)
     messages.success(request, "You have successful logged out!")
     return redirect('homepage')
 
+# def signup_request(request):
+#     if request.method == "POST":
+#         form = SignupForm(request.POST)
+#         if form.is_valid():
+#             user = form.save(commit=False)
+#             user.email = form.cleaned_data["email"]
+#             user.first_name = form.cleaned_data["first_name"]
+#             user.last_name = form.cleaned_data["last_name"]
+#             user.dob = form.cleaned_data["dob"]
+#             user.save()         #Save user instance
+#             messages.success(request, "Signup successful!")
+#             return HttpResponseRedirect("login")
+#     else:
+#         form = SignupForm()
+#     return render(request, "onlinecourse/pages/signup.html", {"form":form})
+
 def signup_request(request):
     if request.method == "POST":
-        form = SignupForm(request.POST)
-        if form.is_valid():
-            user = form.save(commit=False)
-            user.email = form.cleaned_data["email"]
-            user.first_name = form.cleaned_data["first_name"]
-            user.last_name = form.cleaned_data["last_name"]
-            user.dob = form.cleaned_data["dob"]
-            user.save()         #Save user instance
-            messages.success(request, "Signup successful!")
-            return redirect("login")
-    else:
-        form = SignupForm()
-    return render(request, "onlinecourse/signup.html", {"form":form})
+        first_name = request.POST.get("first_name")
+        last_name = request.POST.get("last_name")
+        email = request.POST.get("email")
+        username = request.POST.get("username")
+        dob = request.POST.get("dob")
+        password1 = request.POST.get("password1")
+        password2 = request.POST.get("password2")
+
+        #Basic Input Validation
+        if password1 != password2:
+            return render(request, 'onlinecourse/pages/signup.html', {"signup_error_message": "Passwords do not match!"})
+        if CustomUser.objects.filter(username=username).exists():
+            return render(request, 'onlinecourse/pages/signup.html', {"signup_error_message": "Username already taken!"})
+        if CustomUser.objects.filter(email=email).exists():
+            return render(request, 'onlinecourse/pages/signup.html', {"signup_error_message": "User exists! Please Login"})
+
+        #Create user
+        user = CustomUser.objects.create_user(username=username, email=email, password=password1)
+        user.first_name = first_name
+        user.last_name = last_name
+        user.dob = dob
+        user.save()
+
+        #Return Success and Redirect the user
+        messages.success(request, "Signup successful!")
+        return redirect('login')
+    return render(request,'onlinecourse/pages/signup.html' )
+
+def course_management(request):
+    return render(request, 'onlinecourse/pages/course_management.html')
